23cd106d3c3f3ea73a654dca78be64c7d03ec7f3
kobbyrythm <laryeaisaacbiz@gmail.com>
2022-05-06 20:37:27 +0100

    testing models

--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -1,10 +1,29 @@
 from http import client
 from django.test import TestCase, Client
-from shop.models import Customer,Product,Cart,Order
+from shop.models import Product,Cart,Order
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 class TestCustomerModel(TestCase):
 
-    def SetUp(self):
-        client= Client()
-        response = client.get('/')
-        self.assertEqual(response.status_code,200)
+    def setUp(self):
+        self.data1 = Product.objects.create(name='django')
+
+
+    def test_index_url(self):
+    
+        # Test  model URL reverse
+        data = self.data1
+        response = self.client.post(
+            reverse('shop:index', args=[data.slug]))
+        self.assertEqual(response.status_code, 200)
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Product.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                                            slug='django-beginners', price='20.00', image='django')
+        self.data2 = Product.products.create(category_id=1, title='django advanced', created_by_id=1,
+                                             slug='django-advanced', price='20.00', image='django', is_active=False)
